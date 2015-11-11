ea94c8d6264d5f1d70899f77af516f17b0321603
Damien Maillard <dmaillard06@gmail.com>
2015-11-11 17:44:55 +0100

    add tests

--- test/0-get.js
+++ test/0-get.js
@@ -5,8 +5,12 @@ import fileService from '../index.js';
 var rest = Rest.create();
 rest.use(fileService);
 
+function getFileURL(filepath){
+	return rest.locate('files/' + filepath);
+}
+
 function fetch(filepath){	
-	return rest.fetch(rest.locate('files/' + filepath));
+	return rest.fetch(getFileURL(filepath));
 }
 
 export function suite(add){
@@ -15,36 +19,45 @@ export function suite(add){
 		return this.resolveWith(fetch('200.txt'), {status: 200});
 	});
 
+	add("status 403 for EPERM", function(){
+		return this.resolveWith(fetch('EPERM'), {status: 403});
+	});
+
 	add("status 403 for EACCES", function(){
 
-	});
+	}).skip('no idea how to trigger EACCES error');	
 
-	add("status 403 for EPERM", function(){
-
-	});
-
-	add("status 404 for ENONENT", function(){
-
+	add("status 404 for ENOENT", function(){
+		return this.resolveWith(fetch('ENOENT'), {status: 404});
 	});
 
 	add("status 503 for EBUSY", function(){
+		var fileURL = getFileURL('EBUSY');
+		var filePath = String(fileURL).slice('file:///'.length);
 
-	});
+		return this.resolveWith(fetch('EBUSY'), {status: 503});
+	}).skip('no iead how to lock a file');
 
 	add("status 503 for EMFILE", function(){
 
-	});	
+	}).skip('I wont open 2000 files to test this');
 
 	add("response body is file content", function(){
-
+		return this.resolveWith(fetch('200.txt').then(function(response){
+			return response.text();
+		}), 'Hello world');
 	});
 
 	add("content-type computed from file extension", function(){
-
+		return this.resolveWith(fetch('200.txt').then(function(response){
+			return response.headers.get('content-type');
+		}), 'text/plain');
 	});
 
 	add("last-modified header", function(){
-
+		return this.resolveWith(fetch('200.txt').then(function(response){
+			return response.headers.has('last-modified');
+		}), true);
 	});
 
 	/*
