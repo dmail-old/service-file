284fad30806183812b04b9707f868a2ee8058fe3
Damien Maillard <dmaillard06@gmail.com>
2015-11-14 14:15:14 +0100

    update to use method

--- test/0-get.js
+++ test/0-get.js
@@ -5,22 +5,18 @@ import fileService from '../index.js';
 var rest = Rest.create();
 rest.use(fileService);
 
-function getFileURL(filepath){
-	return rest.locate('files/' + filepath);
-}
-
-function fetch(filepath){	
-	return rest.fetch(getFileURL(filepath));
-}
+console.log('before', rest.baseURL.href);
+rest.baseURL+= 'files/';
+console.log('after', rest.baseURL.href);
 
 export function suite(add){
 
 	add("status 200 for existing file", function(){
-		return this.resolveWith(fetch('200.txt'), {status: 200});
+		return this.resolveWith(rest.get('200.txt'), {status: 200});
 	});
 
 	add("status 403 for EPERM", function(){
-		return this.resolveWith(fetch('EPERM'), {status: 403});
+		return this.resolveWith(rest.get('EPERM'), {status: 403});
 	});
 
 	add("status 403 for EACCES", function(){
@@ -28,14 +24,11 @@ export function suite(add){
 	}).skip('no idea how to trigger EACCES error');	
 
 	add("status 404 for ENOENT", function(){
-		return this.resolveWith(fetch('ENOENT'), {status: 404});
+		return this.resolveWith(rest.get('ENOENT'), {status: 404});
 	});
 
 	add("status 503 for EBUSY", function(){
-		var fileURL = getFileURL('EBUSY');
-		var filePath = String(fileURL).slice('file:///'.length);
-
-		return this.resolveWith(fetch('EBUSY'), {status: 503});
+		return this.resolveWith(rest.get('EBUSY'), {status: 503});
 	}).skip('no iead how to lock a file');
 
 	add("status 503 for EMFILE", function(){
@@ -43,19 +36,19 @@ export function suite(add){
 	}).skip('I wont open 2000 files to test this');
 
 	add("response body is file content", function(){
-		return this.resolveWith(fetch('200.txt').then(function(response){
+		return this.resolveWith(rest.get('200.txt').then(function(response){
 			return response.text();
 		}), 'Hello world');
 	});
 
 	add("content-type computed from file extension", function(){
-		return this.resolveWith(fetch('200.txt').then(function(response){
+		return this.resolveWith(rest.get('200.txt').then(function(response){
 			return response.headers.get('content-type');
 		}), 'text/plain');
 	});
 
 	add("last-modified header", function(){
-		return this.resolveWith(fetch('200.txt').then(function(response){
+		return this.resolveWith(rest.get('200.txt').then(function(response){
 			return response.headers.has('last-modified');
 		}), true);
 	});
